1eff2ab9184b272014e25ff93d986d561e3912cd
Aarav-deepak-jain <88498559+Aarav-deepak-jain@users.noreply.github.com>
2023-12-24 12:38:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+function modelLoaded() {
+    console.log("posenet is initialized!");
+}
+
+function draw() {
+
+    document.getElementById("font").innerHTML = "Width and Height will be = "+difference+"px";
+
+    background('#808080');
+    textSize(difference);
+    fill("#000000");
+    text("Aarav", noseX, noseY);
+
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose X = "+noseX+" nose Y = "+noseY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("rightWrist X = "+ rightWristX+"leftWrist X = "+ leftWristX+"difference = "+ difference );
+
+    }
+}
